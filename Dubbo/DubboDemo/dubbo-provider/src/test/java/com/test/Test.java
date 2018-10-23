c0d22e74115ab109b0d93230964204f47e5a5995
Your Name <15013851179@163.com>
2018-10-23 11:46:46 +0800

    添加dubbo工程

---
+++ Dubbo/DubboDemo/dubbo-provider/src/test/java/com/test/Test.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+/**
+ * 启动远程服务
+ * @author Administrator
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:springmvc.xml");
+        context.start();
+        
+        System.out.println("Dubbo provider start...");
+        try {
+            System.in.read();   // 按任意键退出
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+	}
+}
